b324716ce7b225ba0859b4435a9097179cad4611
Анатолий <toly1917@yandex.ru>
2017-07-01 13:48:22 +0300

    optimization +

--- src/components/Calendar_LeftSide.js
+++ src/components/Calendar_LeftSide.js
@@ -3,7 +3,22 @@ import React from "react";
 
 const { Card, CardTitle, CardText } = MDL;
 
-export default function(props) {
+export default class extends React.PureComponent {
+	render() {
+		return <div>
+			<Card>
+				<CardTitle className="dayNumClass">{this.props.week}</CardTitle>
+				<CardText className="dayClass">
+					<h1>{this.props.day}</h1>
+					<div className="dayClass_month">{this.props.smallMonth}</div>
+					<div className="dayClass_year">{this.props.year}</div>
+				</CardText>
+			</Card>
+		</div>
+	}
+}
+/*function (props) {
+	console.log("render left side");
 	return <div>
 		<Card>
 			<CardTitle className="dayNumClass">{props.week}</CardTitle>
@@ -15,3 +30,4 @@ export default function(props) {
 		</Card>
 	</div>
 }
+*/
